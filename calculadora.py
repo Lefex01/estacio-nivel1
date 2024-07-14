19791173143dab488c73cde2a4089e64d8aaa705
Lefex01 <lucasguimaraes057@gmail.com>
2024-07-14 18:47:23 -0300

    primeiro commit

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+a = int(input('Digite um número:'))
+b = int(input('Digite outro número:'))
+soma = (a + b)
+sub = a - b
+prod = a * b
+div = a / b
+print(f'A soma entre {a} e {b} = {soma}' )
+print(f'A subtração entre {a} e {b} = {sub}')
+print(f'O produto entre {a} e {b} = {prod}')
+print(f'A divisão entre {a} e {b} = {div}')
